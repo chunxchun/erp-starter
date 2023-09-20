95ad113d3a52963819ab331a58e5a60b3c20f29c
William Wu - Mac Mini <william.wu@find2meals.com>
2023-09-21 03:41:05 +0800

    add reminders

---
+++ src/pages/reminders/EditReminder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditReminder = () => {
+  return (
+    <div>EditReminder</div>
+  )
+}
+
+export default EditReminder
---
+++ src/pages/reminders/ListReminder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListReminder = () => {
+  return (
+    <div>ListReminder</div>
+  )
+}
+
+export default ListReminder
