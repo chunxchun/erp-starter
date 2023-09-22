d639a515d2023ea7e397490ea70d7b50ba4065ae
William Wu - Mac Mini <william.wu@find2meals.com>
2023-09-23 01:11:38 +0800

    refactor positions

---
+++ src/pages/positions/ShowPosition.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowPosition = () => {
+  return (
+    <div>ShowPosition</div>
+  )
+}
+
+export default ShowPosition
---
+++ src/pages/positions/index.ts
@@ -0,0 +1,6 @@
+import CreatePosition from "./CreatePosition";
+import EditPosition from "./EditPosition";
+import ListPosition from "./ListPosition";
+import ShowPosition from "./ShowPosition";
+
+export { CreatePosition, EditPosition, ListPosition, ShowPosition };
