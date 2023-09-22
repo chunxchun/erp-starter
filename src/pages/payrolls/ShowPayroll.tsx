9114b6549594946571cbca260f963d3d9cbaf6d0
William Wu - Mac Mini <william.wu@find2meals.com>
2023-09-23 01:12:14 +0800

    refactor payrolls

---
+++ src/pages/payrolls/ShowPayroll.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowPayroll = () => {
+  return (
+    <div>ShowPayroll</div>
+  )
+}
+
+export default ShowPayroll
---
+++ src/pages/payrolls/index.ts
@@ -0,0 +1,6 @@
+import CreatePayroll from "./CreatePayroll";
+import EditPayroll from "./EditPayroll";
+import ListPayroll from "./ListPayroll";
+import ShowPayroll from "./ShowPayroll";
+
+export { CreatePayroll, EditPayroll, ListPayroll, ShowPayroll };
