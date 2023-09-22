69ab5b0af9781a78aaab389598d78f0967d48979
William Wu - Mac Mini <william.wu@find2meals.com>
2023-09-23 01:11:59 +0800

    refactor leaves

---
+++ src/pages/leaves/ShowLeave.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowLeave = () => {
+  return (
+    <div>ShowLeave</div>
+  )
+}
+
+export default ShowLeave
---
+++ src/pages/leaves/index.ts
@@ -0,0 +1,6 @@
+import CreateLeave from "./CreateLeave";
+import EditLeave from "./EditLeave";
+import ListLeave from "./ListLeave";
+import ShowLeave from "./ShowLeave";
+
+export { CreateLeave, EditLeave, ListLeave, ShowLeave };
