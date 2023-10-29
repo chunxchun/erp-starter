766cf382125e17cc72148c81913b800cfad69782
William Wu - Mac Mini <william.wu@find2meals.com>
2023-10-30 00:39:25 +0800

    update landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,20 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useTranslation } from 'react-i18next';
-import { supabase } from '../supabaseClient';
+import { useTranslation } from "react-i18next";
+import { supabase } from "../supabaseClient";
 
 const Landing = () => {
-  const {t} = useTranslation('landing')
+  const { t } = useTranslation("landing");
 
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <p>{t('desc')}</p>
-      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1>{t("title")}</h1>
+      <p>{t("desc")}</p>
+      <div>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
