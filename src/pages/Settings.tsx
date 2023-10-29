b4448d0cdcb7c97946fff55b1e50aac51752058d
William Wu - Mac Mini <william.wu@find2meals.com>
2023-10-30 00:39:32 +0800

    update setting page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,26 @@
-import SignOutButton from "@/components/SignoutButton";
+import SignOutButton from "@/components/SignOutButton";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { supabase } from "@/supabaseClient";
+import { User } from "@supabase/supabase-js";
 
 const Settings = () => {
+  const { t } = useTranslation("settings");
+  const [user, setUser] = useState<null | User>(null);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (error) throw error;
+      console.log(data);
+      setUser(data.user);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <h1>Settings</h1>
+      <h2>user:{user? user.email : 'n/a'}</h2>
       <SignOutButton />
     </div>
   );
